0a11796cfc5d391ba59c0bc4c4139ea93d250da0
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-06-04 15:21:49 +0300

    updates in Frame properties

--- src/components/RightSideBar/RightPanels/FrameProperties/index.tsx
+++ src/components/RightSideBar/RightPanels/FrameProperties/index.tsx
@@ -4,7 +4,7 @@ import type {
   CanvasElementUnion,
   CanvasFrameElement,
 } from "@/features/canvas/types";
-import { TextInput } from "@/components/ui/controlled-inputs/TextInput";
+// import { TextInput } from "@/components/ui/controlled-inputs/TextInput";
 import PositionProperties from "../CommonProperties/PositionProperties";
 import ScaleProperties from "../CommonProperties/ScaleProperties";
 import RotationProperties from "../CommonProperties/RotationProperties";
@@ -17,7 +17,7 @@ import {
   usePostFrameTagMutation,
 } from "@/services/frameTagsApi";
 import { useGetFrameTypesQuery } from "@/services/frameTypesApi";
-import { useGetFramePositionQuery } from "@/services/frmaPosisionApi";
+// import { useGetFramePositionQuery } from "@/services/frmaPosisionApi";
 
 export function FrameProperties({ element }: { element: CanvasFrameElement }) {
   const {
@@ -25,11 +25,12 @@ export function FrameProperties({ element }: { element: CanvasFrameElement }) {
     isLoading: tagsLoading,
     error: errorTags,
   } = useGetFrameTagsQuery();
-  const {
-    data: positionData,
-    isLoading: positionLoading,
-    error: errorPosition,
-  } = useGetFramePositionQuery();
+
+  // const {
+  //   data: positionData,
+  //   isLoading: positionLoading,
+  //   error: errorPosition,
+  // } = useGetFramePositionQuery();
   const { data: typesData, isLoading: typesLoading } = useGetFrameTypesQuery();
   // Normalize typesData to options format
   const typeOptions = typesData
@@ -130,18 +131,19 @@ export function FrameProperties({ element }: { element: CanvasFrameElement }) {
               }
             }}
           />
-          <div className="col-span-full">
+          {/* <div className="col-span-full">
             <TextInput
               label="Label"
               type="text"
               value={element.label}
               onChange={(val) => update({ label: val })}
             />
-          </div>
-          <SelectInput
+          </div> */}
+
+          {/* <SelectInput
             isSearchable
             className="col-span-full"
-            label="position"
+            label="Frame-position"
             value={String(element.position)}
             options={
               Array.isArray(positionData)
@@ -156,7 +158,24 @@ export function FrameProperties({ element }: { element: CanvasFrameElement }) {
             isLoading={positionLoading}
             error={errorPosition ? "Somethin error happen" : ""}
             placeholder="select position..."
+          /> */}
+
+          <SelectInput
+            isSearchable
+            className="col-span-full"
+            label="Frame-position"
+            value={String(element.position)}
+            options={Array.from({ length: 9 }, (_, i) => String(i + 1))}
+            onChange={(val) => {
+              if (typeof val === "string") {
+                update({ position: val });
+              }
+            }}
+            // isLoading={positionLoading}
+            // error={errorPosition ? "Something error happen" : ""}
+            placeholder="select position..."
           />
+
           <SelectInput
             creatable
             isMulti
