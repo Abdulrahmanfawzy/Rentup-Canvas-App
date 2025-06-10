b028d98ba009c57a99e4b312f65d8f72ffadbd7a
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-06-10 13:23:08 +0300

    link frame with an image in layersPanel

--- src/features/canvas/canvasSlice.ts
+++ src/features/canvas/canvasSlice.ts
@@ -61,8 +61,12 @@ const canvasSlice = createSlice({
   initialState,
   reducers: {
     addElement: (
-  state,
-  action: PayloadAction<| { type: "icon"; iconName: string } | { type: Exclude<ElementType, "icon"> }>) => {
+      state,
+      action: PayloadAction<
+        | { type: "icon"; iconName: string }
+        | { type: Exclude<ElementType, "icon"> }
+      >
+    ) => {
       const currentId = shapeIdCounter++;
       const base = createBaseElement(String(currentId));
       let newElement: CanvasElement;
@@ -108,8 +112,6 @@ const canvasSlice = createSlice({
             fill: "transparent",
             dash: [5, 5],
             assetType: "frame",
-            label: "",
-            frame_position: "",
             tags: [],
           } as CanvasElement;
           break;
@@ -118,7 +120,7 @@ const canvasSlice = createSlice({
           newElement = {
             ...base,
             type: "icon",
-            iconName: action.payload.iconName, 
+            iconName: action.payload.iconName,
             width: 50,
             height: 50,
             color: "#000000", // ✅ اللون
@@ -356,7 +358,6 @@ const canvasSlice = createSlice({
         state.past.push(state.elements.map((el) => ({ ...el })));
         state.future = [];
         state.elements[index] = { ...state.elements[index], ...updates };
-        console.log("Updated element:", state.elements[index]);
       }
     },
 
